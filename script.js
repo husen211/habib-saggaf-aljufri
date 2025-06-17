6e42de52040fe0abbc6181dbd80197df4dd455b4
husen211 <huseinaljufri4@gmail.com>
2025-06-18 00:50:28 +0700

    New Item

--- script.js
+++ script.js
@@ -120,7 +120,7 @@ timelineData.forEach((item, index) => {
         try {
             let chatHistory = [{ role: "user", parts: [{ text: promptText }] }];
             const payload = { contents: chatHistory };
-            const apiKey = "";
+            const apiKey = "AIzaSyCUWz7KCQcPAFh_xWOugTeV6JtJBy8laKc";
             const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
             const response = await fetch(apiUrl, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) });
             const result = await response.json();
